f617cb482451165430d0d5debd2b9f59680b110e
Tarunjain-dev <tarun9december@example.com>
2025-02-23 14:07:50 +0530

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Namaste duniya";
+    return 0;
+}
+
+
+/// To run code : press "ctrl + alt + n" together. 
